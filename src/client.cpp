9793e0148f10bde9f15d24839b74c97b11b0833c
mindotale <mindotale@gmail.com>
2022-11-15 18:30:12 +0200

    Clean up code

--- src/client.cpp
+++ src/client.cpp
@@ -1,47 +1,54 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
+
 #include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
- 
-int main(int argc, char const* argv[])
+
+int main(int argc, char const *argv[])
 {
-    int server_socket_fd = 0, client_socket_fd, recv_value;
+    int server_socket_fd, client_socket_fd, recv_value;
     struct sockaddr_in server_addr;
 
-    char buffer[BUFFER_SIZE] = { 0 };
-    char* hello_msg = "Hello from client";
+    char buffer[BUFFER_SIZE] = {0};
+    char *hello_msg = "Hello from client";
 
-    if ((server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_fd < 0)
+    {
         printf("\n Socket creation error \n");
         return -1;
     }
- 
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
- 
+
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0)
+    {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
- 
-    if ((client_socket_fd = connect(
-        server_socket_fd,
-        (struct sockaddr*)&server_addr,
-        sizeof(server_addr))) < 0) {
+
+    client_socket_fd = connect(
+            server_socket_fd,
+            (struct sockaddr *)&server_addr,
+            sizeof(server_addr));
+    if (client_socket_fd < 0)
+    {
         printf("\nConnection Failed \n");
         return -1;
     }
+
     send(server_socket_fd, hello_msg, strlen(hello_msg), 0);
     printf("Hello message sent\n");
     recv_value = recv(server_socket_fd, buffer, BUFFER_SIZE, 0);
     printf("%s\n", buffer);
- 
-    // closing the connected socket
+
+    // Closing the connected socket
     close(client_socket_fd);
     return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,77 +1,85 @@
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
+
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 #define QUEUE_SIZE 8
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
     int server_socket_fd, client_socket_fd, recv_value;
     struct sockaddr_in server_addr;
     int server_addr_len = sizeof(server_addr);
     int opt = 1;
 
-    char buffer[BUFFER_SIZE] = { 0 };
-    char* hello_msg = "Hello from server";
- 
+    char buffer[BUFFER_SIZE] = {0};
+    char *hello_msg = "Hello from server";
+
     // Creating socket file descriptor
-    if ((server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
+    server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_fd < 0)
+    {
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
- 
+
     // Forcefully attaching socket to the port PORT
     if (setsockopt(
-        server_socket_fd, SOL_SOCKET,
-        SO_REUSEADDR | SO_REUSEPORT, 
-        &opt,
-        sizeof(opt))) {
-        perror("setsockopt");
+            server_socket_fd, SOL_SOCKET,
+            SO_REUSEADDR | SO_REUSEPORT,
+            &opt,
+            sizeof(opt)))
+    {
+        perror("Setsockopt failed");
         exit(EXIT_FAILURE);
     }
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
- 
+
     // Forcefully attaching socket to the port PORT
     if (bind(
-        server_socket_fd,
-        (struct sockaddr*)&server_addr,
-        sizeof(server_addr)) < 0) {
-        perror("bind failed");
+            server_socket_fd,
+            (struct sockaddr *)&server_addr,
+            sizeof(server_addr)) < 0)
+    {
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_socket_fd, QUEUE_SIZE) < 0) {
-        perror("listen");
+    if (listen(server_socket_fd, QUEUE_SIZE) < 0)
+    {
+        perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    while(true)
+    while (true)
     {
         client_socket_fd = accept(
             server_socket_fd,
-            (struct sockaddr*)&server_addr,
-            (socklen_t*)&server_addr_len);
-        if (client_socket_fd < 0) {
-            perror("accept");
+            (struct sockaddr *)&server_addr,
+            (socklen_t *)&server_addr_len);
+        if (client_socket_fd < 0)
+        {
+            perror("Accept failed");
             exit(EXIT_FAILURE);
         }
+
         recv_value = recv(client_socket_fd, buffer, BUFFER_SIZE, 0);
         printf("%s\n", buffer);
         send(client_socket_fd, hello_msg, strlen(hello_msg), 0);
         printf("Hello message sent\n");
-    
-        // closing the connected socket
+
+        // Closing the connected socket
         close(client_socket_fd);
     }
-    
-    // closing the listening socket
+
+    // Closing the listening socket
     shutdown(server_socket_fd, SHUT_RDWR);
     return 0;
 }
